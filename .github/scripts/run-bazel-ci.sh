f1dd8107ee9f648ec753fbc6317ab36bd5eaf149
starr-openai <starr@openai.com>
2026-04-14 20:43:52 -0700

    Keep remote downloader config in local Bazel fallback

--- .github/scripts/run-bazel-ci.sh
+++ .github/scripts/run-bazel-ci.sh
@@ -251,14 +251,10 @@ else
   #   clear remote cache/execution endpoints configured in .bazelrc.
   #   https://bazel.build/reference/command-line-reference#common_options-flag--remote_cache
   #   https://bazel.build/reference/command-line-reference#common_options-flag--remote_executor
-  # --experimental_remote_downloader=:
-  #   clear the remote downloader endpoint because Bazel requires it to be used
-  #   with a gRPC remote cache.
   bazel_run_args=(
     "${bazel_args[@]}"
     --remote_cache=
     --remote_executor=
-    --experimental_remote_downloader=
   )
   if (( ${#post_config_bazel_args[@]} > 0 )); then
     bazel_run_args+=("${post_config_bazel_args[@]}")
